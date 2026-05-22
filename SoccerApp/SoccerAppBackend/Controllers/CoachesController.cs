77a52737f2c129b465e2369ebb9be201a62b3e69
Phillip Martinez <phillip.martinez210@gmail.com>
2026-05-22 11:56:05 -0500

    Created primary constructor and DI Database

--- SoccerApp/SoccerAppBackend/Controllers/CoachesController.cs
+++ SoccerApp/SoccerAppBackend/Controllers/CoachesController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SoccerAppBackend.Data;
 
 namespace SoccerAppBackend.Controllers
 {
@@ -6,6 +7,14 @@ namespace SoccerAppBackend.Controllers
     [Route("api/[controller]")]
     public class CoachesController : Controller
     {
+        private readonly IDatabase database;
+
+        public CoachesController(IDatabase database)
+        {
+            this.database = database;
+        }
+
+
         [HttpGet]
         public IActionResult Index()
         {
