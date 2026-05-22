44597a1fd75db9682d4017f4dd1695cea8b9e5b0
Phillip Martinez <phillip.martinez210@gmail.com>
2026-05-22 15:13:06 -0500

    Updates to Coach model

--- SoccerApp/SoccerAppBackend/Models/Coach.cs
+++ SoccerApp/SoccerAppBackend/Models/Coach.cs
@@ -3,11 +3,11 @@
     public class Coach
     {
         public int CoachId { get; set; }
-        public string CoachingLicense { get; set; }
-        public DateOnly StartedCoachingDate { get; set; }
-        public int IsActive { get; set; }
-        public int UserId { get; set; }
+        public string? CoachingLicense { get; set; }
+        public DateTime StartedCoachingDate { get; set; }
+        public bool IsActive { get; set; }
+        public int? UserId { get; set; }
         public DateTime CreatedAt { get; set; }
-        public DateTime ModifiedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
     }
 }
