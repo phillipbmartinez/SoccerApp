95b13e40046d81ddae4a887caddb524c67869822
Phillip Martinez <phillip.martinez210@gmail.com>
2026-05-22 12:43:59 -0500

    Created new Coach class

---
+++ SoccerApp/SoccerAppBackend/Models/Coach.cs
@@ -0,0 +1,13 @@
+﻿namespace SoccerAppBackend.Models
+{
+    public class Coach
+    {
+        public int CoachId { get; set; }
+        public string CoachingLicense { get; set; }
+        public DateOnly StartedCoachingDate { get; set; }
+        public int IsActive { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime ModifiedAt { get; set; }
+    }
+}
