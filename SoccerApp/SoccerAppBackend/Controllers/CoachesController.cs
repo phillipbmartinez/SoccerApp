8e371fcb3f60f004cfa2a548030867c7e1fdc5b3
Phillip Martinez <phillip.martinez210@gmail.com>
2026-05-22 11:26:48 -0500

    Created CoachesController

---
+++ SoccerApp/SoccerAppBackend/Controllers/CoachesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoccerAppBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CoachesController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
