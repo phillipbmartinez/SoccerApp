605ae46e75d8b94bb96fa173d2fe476bfc0b6454
Phillip Martinez <phillip.martinez210@gmail.com>
2026-05-26 15:23:53 -0500

    Added ReturnAllInactiveCoaches method to CoachesService class and interface

--- SoccerApp/SoccerAppBackend/Data/CoachesService.cs
+++ SoccerApp/SoccerAppBackend/Data/CoachesService.cs
@@ -64,6 +64,56 @@ namespace SoccerAppBackend.Data
             }
         }
 
+        public async Task<List<Coach>> ReturnAllInactiveCoaches()
+        {
+            List<Coach> coaches = new List<Coach>();
+
+            string sqlQuery = "SELECT * FROM SoccerAppCoaches WHERE IsActive = 0";
+
+            try
+            {
+                using SqlConnection connection = databaseSerivce.CreateDbConnection();
+                await connection.OpenAsync();
+                using SqlCommand command = new SqlCommand(sqlQuery, connection);
+                SqlDataReader reader = await command.ExecuteReaderAsync();
+
+                while (await reader.ReadAsync())
+                {
+                    coaches.Add(new Coach
+                    {
+                        CoachId = reader.GetInt32(reader.GetOrdinal("CoachId")),
+                        CoachingLicense = reader.IsDBNull(reader.GetOrdinal("CoachingLicense"))
+                        ? null
+                        : reader.GetString(reader.GetOrdinal("CoachingLicense")),
+                        StartedCoachingDate = reader.GetDateTime(reader.GetOrdinal("StartedCoachingDate")),
+                        IsActive = reader.GetBoolean(reader.GetOrdinal("IsActive")),
+                        UserId = reader.IsDBNull(reader.GetOrdinal("UserId"))
+                        ? null
+                        : reader.GetInt32(reader.GetOrdinal("UserId")),
+                        CreatedAt = reader.GetDateTime(reader.GetOrdinal("CreatedAt")),
+                        ModifiedAt = reader.IsDBNull(reader.GetOrdinal("ModifiedAt"))
+                        ? null
+                        : reader.GetDateTime(reader.GetOrdinal("ModifiedAt")),
+                    });
+                }
+                ;
+
+                return coaches;
+            }
+            catch (SqlException sqlEx)
+            {
+                Console.WriteLine($"[SQL EXCEPTION thrown from SoccerAppBackend => CoachesService => ReturnAllInactiveCoaches: {DateTime.Now}] - SQL Exception: {sqlEx.Message}");
+                Console.WriteLine(sqlEx.StackTrace);
+                return coaches;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[EXCEPTION thrown from SoccerAppBackend => CoachesService => ReturnAllInactiveCoaches: {DateTime.Now}] - Exception: {ex.Message}");
+                Console.WriteLine(ex.StackTrace);
+                return coaches;
+            }
+        }
+
 
         public async Task<Coach> ReturnCoachById(int coachId)
         {
--- SoccerApp/SoccerAppBackend/Data/ICoachesService.cs
+++ SoccerApp/SoccerAppBackend/Data/ICoachesService.cs
@@ -5,6 +5,7 @@ namespace SoccerAppBackend.Data
     public interface ICoachesService
     {
         Task<List<Coach>> ReturnAllActiveCoaches();
+        Task<List<Coach>> ReturnAllInactiveCoaches();
         Task<Coach> ReturnCoachById(int coachId);
     }
 }
