ce82c1299379ed842d5d8ceeddca9b0a93c9e103
Phillip Martinez <phillip.martinez210@gmail.com>
2026-06-04 15:00:02 -0500

    Created User model

---
+++ SoccerApp/SoccerAppBackend/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace SoccerAppBackend.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public int IsActive { get; set; }
+        public int? RoleId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+    }
+}
