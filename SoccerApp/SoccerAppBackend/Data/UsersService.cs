27f3f6fb767e894465ba40876a57923527424fad
Phillip Martinez <phillip.martinez210@gmail.com>
2026-06-04 14:52:26 -0500

    Created UsersService class and interface

---
+++ SoccerApp/SoccerAppBackend/Data/UsersService.cs
@@ -0,0 +1,12 @@
+﻿namespace SoccerAppBackend.Data
+{
+    public class UsersService : IUsersService
+    {
+        private readonly IDatabaseService databaseSerivce;
+
+        public UsersService(IDatabaseService databaseSerivce)
+        {
+            this.databaseSerivce = databaseSerivce;
+        }
+    }
+}
