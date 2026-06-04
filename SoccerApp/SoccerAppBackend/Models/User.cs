dfcfb234878e25dc404c3f3c1ef57ba83acebb07
Phillip Martinez <phillip.martinez210@gmail.com>
2026-06-04 15:12:04 -0500

    Updated IsActive to a bool

--- SoccerApp/SoccerAppBackend/Models/User.cs
+++ SoccerApp/SoccerAppBackend/Models/User.cs
@@ -7,7 +7,7 @@
         public string LastName { get; set; }
         public string Email { get; set; }
         public DateTime DateOfBirth { get; set; }
-        public int IsActive { get; set; }
+        public bool IsActive { get; set; }
         public int? RoleId { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? ModifiedAt { get; set; }
