73630a31894fb5a559e05e294bb1fbb2f72d3e8c
Phillip Martinez <phillip.martinez210@gmail.com>
2026-06-04 14:54:29 -0500

    Created UserController and injected database and users services

---
+++ SoccerApp/SoccerAppBackend/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoccerAppBackend.Data;
+
+namespace SoccerAppBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        private readonly IDatabaseService databaseService;
+        private readonly IUsersService usersService;
+
+        public UsersController(IDatabaseService databaseService, IUsersService usersService)
+        {
+            this.databaseService = databaseService;
+            this.usersService = usersService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
