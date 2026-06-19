d5444044190eff7071d20297df889a4e70cb9309
Phillip Martinez <phillip.martinez210@gmail.com>
2026-06-19 15:12:54 -0500

    Added RolesService calss and interface

---
+++ SoccerApp/SoccerAppBackend/Data/RolesService.cs
@@ -0,0 +1,12 @@
+﻿namespace SoccerAppBackend.Data
+{
+    public class RolesService : IRolesService
+    {
+        private readonly IDatabaseService databaseService;
+
+        public RolesService(IDatabaseService databaseService)
+        {
+            this.databaseService = databaseService;
+        }
+    }
+}
