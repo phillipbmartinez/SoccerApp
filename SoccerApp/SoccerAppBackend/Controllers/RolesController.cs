e937018ad940b8243bfe689299a08259aeae133f
Phillip Martinez <phillip.martinez210@gmail.com>
2026-06-19 16:30:16 -0500

    Added RolesController with GetRoles action method

---
+++ SoccerApp/SoccerAppBackend/Controllers/RolesController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoccerAppBackend.Data;
+using SoccerAppBackend.Models;
+
+namespace SoccerAppBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class RolesController : Controller
+    {
+        private readonly IDatabaseService databaseService;
+        private readonly IRolesService rolesService;
+
+        public RolesController(IDatabaseService databaseService, IRolesService rolesService)
+        {
+            this.databaseService = databaseService;
+            this.rolesService = rolesService;
+        }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetRoles()
+        {
+            List<RoleDto> roles = await rolesService.GetRoles();
+
+            return Ok(roles);
+        }
+    }
+}
