529b61139632ad34f9fcf3f36b06fc10b12f8a4c
Phillip Martinez <phillip.martinez210@gmail.com>
2026-06-24 08:40:25 -0500

    Created new PlayerDto model

---
+++ SoccerApp/SoccerAppBackend/Models/PlayerDto.cs
@@ -0,0 +1,13 @@
+﻿namespace SoccerAppBackend.Models
+{
+    public class PlayerDto
+    {
+        public int PlayerId { get; set; }
+        public int? UserId { get; set; }
+        public int? TeamId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int? JerseyNumber { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
