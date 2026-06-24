987e4f9d795838437d918d1b0fbe275707387e4c
Phillip Martinez <phillip.martinez210@gmail.com>
2026-06-24 09:07:04 -0500

    Created Players controller and added GetActivePlayers action method

---
+++ SoccerApp/SoccerAppBackend/Controllers/PlayersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SoccerAppBackend.Data;
+using SoccerAppBackend.Models;
+
+namespace SoccerAppBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PlayersController : Controller
+    {
+        private readonly IPlayersService playersService;
+
+        public PlayersController(IPlayersService playersService)
+        {
+            this.playersService = playersService;
+        }
+
+
+        public async Task<IActionResult> GetActivePlayers()
+        {
+            List<PlayerDto> activePlayers = await playersService.GetActivePlayers();
+
+            return Ok(activePlayers);
+        }
+    }
+}
