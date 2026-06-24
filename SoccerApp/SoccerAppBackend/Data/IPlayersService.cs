cbe60cd2ec8eab7e2a2fd1909b15cc6ec6823fb2
Phillip Martinez <phillip.martinez210@gmail.com>
2026-06-24 16:45:18 -0500

    Added GetPlayerById and CreatePlayer methods to class and interface

--- SoccerApp/SoccerAppBackend/Data/IPlayersService.cs
+++ SoccerApp/SoccerAppBackend/Data/IPlayersService.cs
@@ -4,6 +4,8 @@ namespace SoccerAppBackend.Data
 {
     public interface IPlayersService
     {
+        Task<PlayerDto> CreatePlayer(PlayerDto playerToCreate);
         Task<List<PlayerDto>> GetActivePlayers();
+        Task<PlayerDto> GetPlayerById(int playerId);
     }
 }
--- SoccerApp/SoccerAppBackend/Data/PlayersService.cs
+++ SoccerApp/SoccerAppBackend/Data/PlayersService.cs
@@ -3,7 +3,7 @@ using SoccerAppBackend.Models;
 
 namespace SoccerAppBackend.Data
 {
-    public class PlayersService
+    public class PlayersService : IPlayersService
     {
         private readonly IDatabaseService databaseService;
 
@@ -61,6 +61,140 @@ namespace SoccerAppBackend.Data
                 Console.WriteLine($"[EXCEPTION thrown from SoccerAppBackend => PlayersService => GetPlayers: {DateTime.Now}] - Exception: {ex.Message}");
                 Console.WriteLine(ex.StackTrace);
                 return activePlayers;
+            }
+        }
+
+
+        public async Task<PlayerDto> GetPlayerById(int playerId)
+        {
+            PlayerDto player = new PlayerDto();
+
+            string sqlQuery = @"SELECT * FROM SoccerAppPlayers WHERE PlayerId = @playerId AND IsActive = 1";
+
+            try
+            {
+                using SqlConnection connection = databaseService.CreateDbConnection();
+                await connection.OpenAsync();
+                using SqlCommand command = new SqlCommand(sqlQuery, connection);
+
+                command.Parameters.AddWithValue("@playerId", playerId);
+
+                SqlDataReader reader = await command.ExecuteReaderAsync();
+
+                while (await reader.ReadAsync())
+                {
+                    player.PlayerId = reader.GetInt32(reader.GetOrdinal("PlayerId"));
+                    player.UserId = reader.IsDBNull(reader.GetOrdinal("UserId"))
+                        ? (int?)null
+                        : reader.GetInt32(reader.GetOrdinal("UserId"));
+                    player.TeamId = reader.IsDBNull(reader.GetOrdinal("TeamId"))
+                        ? (int?)null
+                        : reader.GetInt32(reader.GetOrdinal("TeamId"));
+                    player.FirstName = reader.GetString(reader.GetOrdinal("FirstName"));
+                    player.LastName = reader.GetString(reader.GetOrdinal("LastName"));
+                    player.JerseyNumber = reader.IsDBNull(reader.GetOrdinal("JerseyNumber"))
+                        ? (int?)null
+                        : reader.GetInt32(reader.GetOrdinal("JerseyNumber"));
+                    player.DateOfBirth = reader.IsDBNull(reader.GetOrdinal("DateOfBirth"))
+                        ? (DateTime?)null
+                        : reader.GetDateTime(reader.GetOrdinal("DateOfBirth"));
+                }
+                ;
+
+                return player;
+
+            }
+            catch (SqlException sqlEx)
+            {
+                Console.WriteLine($"[SQL EXCEPTION thrown from SoccerAppBackend => PlayersService => GetPlayerById: {DateTime.Now}] - SQL Exception: {sqlEx.Message}");
+                Console.WriteLine(sqlEx.StackTrace);
+                return player;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[EXCEPTION thrown from SoccerAppBackend => PlayersService => GetPlayerById: {DateTime.Now}] - Exception: {ex.Message}");
+                Console.WriteLine(ex.StackTrace);
+                return player;
+            }
+        }
+
+
+        public async Task<PlayerDto> CreatePlayer(PlayerDto playerToCreate)
+        {
+            PlayerDto newPlayer = new PlayerDto();
+
+            string sqlQuery =
+                @"
+                    INSERT INTO SoccerAppPlayers (UserId, TeamId, FirstName, LastName, JerseyNumber, DateOfBirth)
+                    VALUES (@userId, @teamId, @firstName, @lastName, @jerseyNumber, @dateOfBirth)
+                    SELECT CAST(SCOPE_IDENTITY() AS INT);
+                ";
+
+            try
+            {
+                using SqlConnection connection = databaseService.CreateDbConnection();
+                await connection.OpenAsync();
+                using SqlCommand command = new SqlCommand(sqlQuery, connection);
+
+                if (!string.IsNullOrWhiteSpace(playerToCreate.UserId.ToString()))
+                {
+                    command.Parameters.AddWithValue("@userId", playerToCreate.UserId);
+                }
+                else
+                {
+                    command.Parameters.AddWithValue("@userId", DBNull.Value);
+                }
+
+                if (!string.IsNullOrWhiteSpace(playerToCreate.TeamId.ToString()))
+                {
+                    command.Parameters.AddWithValue("@teamId", playerToCreate.TeamId);
+                }
+                else
+                {
+                    command.Parameters.AddWithValue("@teamId", DBNull.Value);
+                }
+
+                command.Parameters.AddWithValue("@firstName", playerToCreate.FirstName);
+                command.Parameters.AddWithValue("@lastName", playerToCreate.LastName);
+
+                if (!string.IsNullOrWhiteSpace(playerToCreate.JerseyNumber.ToString()))
+                {
+                    command.Parameters.AddWithValue("@jerseyNumber", playerToCreate.JerseyNumber);
+                }
+                else
+                {
+                    command.Parameters.AddWithValue("@jerseyNumber", DBNull.Value);
+                }
+
+                command.Parameters.AddWithValue("@dateOfBirth", playerToCreate.DateOfBirth);
+
+                var result = await command.ExecuteScalarAsync();
+
+                if (result != null && int.TryParse(result.ToString(), out int newPlayerId))
+                {
+                    newPlayer = await GetPlayerById(newPlayerId);
+
+                    Console.WriteLine($"[SUCCESS : {DateTime.Now}] - New Plasyer inserted. ID: {newPlayerId} from SoccerAppBackend => PlayersService => CreatePlayer");
+                }
+                else
+                {
+                    Console.WriteLine($"[ERROR : {DateTime.Now}] - Error creating new Player record in database from SoccerAppBackend => PlayersService => CreatePlayer");
+                }
+
+                return newPlayer;
+
+            }
+            catch (SqlException sqlEx)
+            {
+                Console.WriteLine($"[SQL EXCEPTION thrown from SoccerAppBackend => PlayersService => CreatePlayer: {DateTime.Now}] - SQL Exception: {sqlEx.Message}");
+                Console.WriteLine(sqlEx.StackTrace);
+                return newPlayer;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[EXCEPTION thrown from SoccerAppBackend => PlayersService => CreatePlayer: {DateTime.Now}] - Exception: {ex.Message}");
+                Console.WriteLine(ex.StackTrace);
+                return newPlayer;
             }
         }
     }
