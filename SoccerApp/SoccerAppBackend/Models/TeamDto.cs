8c380dd8afecc1ed44406a44994c77c961e8351e
Phillip Martinez <phillip.martinez210@gmail.com>
2026-07-20 15:28:31 -0500

    Created TeamDto class

---
+++ SoccerApp/SoccerAppBackend/Models/TeamDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SoccerAppBackend.Models
+{
+    public class TeamDto
+    {
+        public int TeamId { get; set; }
+        public string TeamName { get; set; }
+        public int CoachId { get; set; }
+        public string AgeGroup { get; set; }
+    }
+}
