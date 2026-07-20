0cb4f1dec6832431c9d57ddd1102e910b57afe6f
Phillip Martinez <phillip.martinez210@gmail.com>
2026-07-20 15:30:11 -0500

    Added TeamsController

---
+++ SoccerApp/SoccerAppBackend/Controllers/TeamsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SoccerAppBackend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TeamsController : Controller
+    {
+        public TeamsController()
+        {
+        
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
