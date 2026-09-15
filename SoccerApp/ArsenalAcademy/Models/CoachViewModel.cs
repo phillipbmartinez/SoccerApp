6dfc86b34d778de61981d3ef5c2e6ffaf1330ad0
Phillip Martinez <phillip.martinez210@gmail.com>
2026-09-15 09:43:43 -0500

    Added CoachViewModel

---
+++ SoccerApp/ArsenalAcademy/Models/CoachViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArsenalAcademy.Models
+{
+    public class CoachViewModel
+    {
+        public int CoachId { get; set; }
+        public string? CoachingLicense { get; set; }
+        public DateTime StartedCoachingDate { get; set; }
+        public bool IsActive { get; set; }
+        public int? UserId { get; set; }
+    }
+}
