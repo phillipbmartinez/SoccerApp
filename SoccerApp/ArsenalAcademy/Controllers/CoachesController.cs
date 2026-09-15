7bf874253d4f9db1db8106039aa8c8482265679f
Phillip Martinez <phillip.martinez210@gmail.com>
2026-09-15 09:43:12 -0500

    Created Coaches controller and added Index and GetCoach action methods that display the corresponding views

---
+++ SoccerApp/ArsenalAcademy/Controllers/CoachesController.cs
@@ -0,0 +1,46 @@
+﻿using ArsenalAcademy.Models;
+using ArsenalAcademy.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArsenalAcademy.Controllers
+{
+    public class CoachesController : Controller
+    {
+        private readonly ICoachesApiService coachesApiService;
+
+        public CoachesController(ICoachesApiService coachesApiService)
+        {
+            this.coachesApiService = coachesApiService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            List<CoachViewModel> coaches = await coachesApiService.GetAllCoaches();
+
+            if (coaches != null && coaches.Count > 0)
+            {
+                return View(coaches);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCoach(int id)
+        {
+            CoachViewModel coach = await coachesApiService.GetCoachById(id);
+
+            if (coach != null && coach.CoachId > 0)
+            {
+                return View(coach);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
