032c00da2fe443f2134ee9fb35d1cf8c63531053
Phillip Martinez <phillip.martinez210@gmail.com>
2026-09-15 09:44:59 -0500

    Added Coaches API class and interface and added GetAllCoaches and GetCoachById methods

---
+++ SoccerApp/ArsenalAcademy/Services/CoachesApiService.cs
@@ -0,0 +1,55 @@
+﻿using System.Net.Http;
+using ArsenalAcademy.Models;
+
+namespace ArsenalAcademy.Services
+{
+    public class CoachesApiService : ICoachesApiService
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public CoachesApiService(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<List<CoachViewModel>> GetAllCoaches()
+        {
+            HttpClient httpClient = httpClientFactory.CreateClient("SoccerAppApi");
+            List<CoachViewModel> coaches = new List<CoachViewModel>();
+
+            try
+            {
+                coaches = await httpClient.GetFromJsonAsync<List<CoachViewModel>>("coaches");
+
+                return coaches;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[EXCEPTION thrown from ArsenalAcademy => CoachesApiService => GetAllCoaches: {DateTime.Now}] - Exception: {ex.Message}");
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            return coaches;
+        }
+
+        public async Task<CoachViewModel> GetCoachById(int coachId)
+        {
+            HttpClient httpClient = httpClientFactory.CreateClient("SoccerAppApi");
+            CoachViewModel coach = new CoachViewModel();
+
+            try
+            {
+                coach = await httpClient.GetFromJsonAsync<CoachViewModel>($"coaches/{coachId}");
+
+                return coach;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[EXCEPTION thrown from ArsenalAcademy => CoachesApiService => GetAllCoaches: {DateTime.Now}] - Exception: {ex.Message}");
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            return coach;
+        }
+    }
+}
---
+++ SoccerApp/ArsenalAcademy/Services/ICoachesService.cs
@@ -0,0 +1,10 @@
+﻿using ArsenalAcademy.Models;
+
+namespace ArsenalAcademy.Services
+{
+    public interface ICoachesApiService
+    {
+        Task<List<CoachViewModel>> GetAllCoaches();
+        Task<CoachViewModel> GetCoachById(int coachId);
+    }
+}
