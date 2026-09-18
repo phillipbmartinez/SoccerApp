84918b352948fb0826c13e9aebc01fe5ced16893
Phillip Martinez <phillip.martinez210@gmail.com>
2026-09-18 14:59:25 -0500

    Added data annotations to model

--- SoccerApp/ArsenalAcademy/Models/CreateCoachViewModel.cs
+++ SoccerApp/ArsenalAcademy/Models/CreateCoachViewModel.cs
@@ -1,12 +1,20 @@
-﻿namespace ArsenalAcademy.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArsenalAcademy.Models
 {
     public class CreateCoachViewModel
     {
+        [Required(ErrorMessage = "First name is required.")]
         public string FirstName { get; set; }
+        [Required(ErrorMessage = "Last name is required.")]
         public string LastName { get; set; }
+        [Required(ErrorMessage = "First name is required.")]
+        [EmailAddress(ErrorMessage = "Must be a valid email address.")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Date of birth is required.")]
         public DateTime DateOfBirth { get; set; }
         public string? CoachingLicense { get; set; }
+        [Required(ErrorMessage = "Coaching date required is required.")]
         public DateTime StartedCoachingDate { get; set; }
         public string? TeamName { get; set; }
     }
