75e86d03d5763349874a0980f20fa292a50f7641
Phillip Martinez <phillip.martinez210@gmail.com>
2026-09-22 16:25:34 -0500

    Added class and interface to get game information

---
+++ SoccerApp/ArsenalAcademy/Services/GameTeamOpponentsApiService.cs
@@ -0,0 +1,34 @@
+﻿using ArsenalAcademy.Models;
+
+namespace ArsenalAcademy.Services
+{
+    public class GameTeamOpponentsApiService : IGameTeamOpponentsApiService
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public GameTeamOpponentsApiService(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<List<ViewGameTeamOpponentViewModel>> GetGames()
+        {
+            HttpClient httpClient = httpClientFactory.CreateClient("SoccerAppApi");
+            List<ViewGameTeamOpponentViewModel> games = new List<ViewGameTeamOpponentViewModel>();
+
+            try
+            {
+                games = await httpClient.GetFromJsonAsync<List<ViewGameTeamOpponentViewModel>>("games");
+
+                return games;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[EXCEPTION thrown from ArsenalAcademy => GameTeamOpponentsApiService => GetGames: {DateTime.Now}] - Exception: {ex.Message}");
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            return games;
+        }
+    }
+}
