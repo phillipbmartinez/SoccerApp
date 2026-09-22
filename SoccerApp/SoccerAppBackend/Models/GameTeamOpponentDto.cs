0fe8cab595510357a4c968947c5326125b4027a8
Phillip Martinez <phillip.martinez210@gmail.com>
2026-09-22 15:42:50 -0500

    Added GameTeamOpponentDto

---
+++ SoccerApp/SoccerAppBackend/Models/GameTeamOpponentDto.cs
@@ -0,0 +1,17 @@
+﻿namespace SoccerAppBackend.Models
+{
+    public class GameTeamOpponentDto
+    {
+        public int GameId { get; set; }
+        public DateTime GameDate { get; set; }
+        public string GameLocation { get; set; }
+        public string? GameStatus { get; set; }
+        public string? AgeGroup { get; set; }
+        public int TeamId { get; set; }
+        public string TeamName { get; set; }
+        public int? TeamScore { get; set; }
+        public int OpponentId { get; set; }
+        public string OpponentName { get; set; }
+        public int? OpponentScore { get; set; }
+    }
+}
