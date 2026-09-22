0eef9fffa610f3d56856e5c87c5685e5034cd298
Phillip Martinez <phillip.martinez210@gmail.com>
2026-09-22 16:09:22 -0500

    Created new view model for gameTeamOpponents

---
+++ SoccerApp/ArsenalAcademy/Models/ViewGameTeamOpponentViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ArsenalAcademy.Models
+{
+    public class ViewGameTeamOpponentViewModel
+    {
+        public int GameId { get; set; }
+        public DateTime GameDate { get; set; }
+        public string? GameLocation { get; set; }
+        public string? GameStatus { get; set; }
+        public string? AgeGroup { get; set; }
+        public int TeamId { get; set; }
+        public string TeamName { get; set; }
+        public int? TeamScore { get; set; }
+        public int OpponentId { get; set; }
+        public string OpponentName { get; set; }
+        public int? OpponentScore { get; set; }
+    }
+}
