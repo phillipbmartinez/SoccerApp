d81c0829db6d2d7b6253a7bc97890a2f8d1f5629
Phillip Martinez <phillip.martinez210@gmail.com>
2026-09-22 16:02:40 -0500

    Created TeamsController for future use

---
+++ SoccerApp/ArsenalAcademy/Controllers/TeamsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArsenalAcademy.Controllers
+{
+    public class TeamsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
