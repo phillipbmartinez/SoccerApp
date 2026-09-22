6aabae982153a2b98cfa83272eca28f712632361
Phillip Martinez <phillip.martinez210@gmail.com>
2026-09-22 16:24:58 -0500

    Created GamesController and added GetGames action method

---
+++ SoccerApp/ArsenalAcademy/Controllers/GamesController.cs
@@ -0,0 +1,23 @@
+﻿using ArsenalAcademy.Models;
+using ArsenalAcademy.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArsenalAcademy.Controllers
+{
+    public class GamesController : Controller
+    {
+        private readonly IGameTeamOpponentsApiService gameTeamOpponentsApiService;
+
+        public GamesController(IGameTeamOpponentsApiService gameTeamOpponentsApiService)
+        {
+            this.gameTeamOpponentsApiService = gameTeamOpponentsApiService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<ViewGameTeamOpponentViewModel> games = await gameTeamOpponentsApiService.GetGames();
+
+            return View(games);
+        }
+    }
+}
