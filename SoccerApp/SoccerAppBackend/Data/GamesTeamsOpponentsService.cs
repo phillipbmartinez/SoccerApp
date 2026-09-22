3ef6004e67951ba5d3df01ab13eda39f8f67d7d1
Phillip Martinez <phillip.martinez210@gmail.com>
2026-09-22 16:00:52 -0500

    Created class and interface and GetGames method for use in GamesController

---
+++ SoccerApp/SoccerAppBackend/Data/GamesTeamsOpponentsService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Data.SqlClient;
+using SoccerAppBackend.Models;
+
+namespace SoccerAppBackend.Data
+{
+    public class GamesTeamsOpponentsService : IGamesTeamsOpponentsService
+    {
+        private readonly IDatabaseService databaseService;
+
+        public GamesTeamsOpponentsService(IDatabaseService databaseService)
+        {
+            this.databaseService = databaseService;
+        }
+
+        public async Task<List<GameTeamOpponentDto>> GetGames()
+        {
+            List<GameTeamOpponentDto> games = new List<GameTeamOpponentDto>();
+
+            string sqlQuery = "SELECT * FROM [vw_SoccerAppGamesTeamsOpponents]";
+
+            try
+            {
+                using SqlConnection connection = databaseService.CreateDbConnection();
+                await connection.OpenAsync();
+                using SqlCommand command = new SqlCommand(sqlQuery, connection);
+                SqlDataReader reader = await command.ExecuteReaderAsync();
+
+                while (await reader.ReadAsync())
+                {
+                    games.Add(new GameTeamOpponentDto
+                    {
+                        GameId = reader.GetInt32(reader.GetOrdinal("GameId")),
+                        GameDate = reader.GetDateTime(reader.GetOrdinal("GameDate")),
+                        GameLocation = reader.IsDBNull(reader.GetOrdinal("GameLocation"))
+                        ? null
+                        : reader.GetString(reader.GetOrdinal("GameLocation")),
+                        GameStatus = reader.GetString(reader.GetOrdinal("GameStatus")),
+                        AgeGroup = reader.IsDBNull(reader.GetOrdinal("AgeGroup"))
+                        ? null
+                        : reader.GetString(reader.GetOrdinal("AgeGroup")),
+                        TeamId = reader.GetInt32(reader.GetOrdinal("TeamId")),
+                        TeamName = reader.GetString(reader.GetOrdinal("TeamName")),
+                        TeamScore = reader.IsDBNull(reader.GetOrdinal("TeamScore"))
+                        ? null
+                        : reader.GetInt32(reader.GetOrdinal("TeamScore")),
+                        OpponentId = reader.GetInt32(reader.GetOrdinal("OpponentId")),
+                        OpponentName = reader.GetString(reader.GetOrdinal("OpponentName")),
+                        OpponentScore = reader.IsDBNull(reader.GetOrdinal("OpponentScore"))
+                        ? null
+                        : reader.GetInt32(reader.GetOrdinal("OpponentScore")),
+                    });
+                };
+
+                return games;
+            }
+            catch (SqlException sqlEx)
+            {
+                Console.WriteLine($"[SQL EXCEPTION thrown from SoccerAppBackend => GamesTeamsOpponentsService => GetGames: {DateTime.Now}] - SQL Exception: {sqlEx.Message}");
+                Console.WriteLine(sqlEx.StackTrace);
+                return games;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[EXCEPTION thrown from SoccerAppBackend => GamesTeamsOpponentsService => GetGames: {DateTime.Now}] - Exception: {ex.Message}");
+                Console.WriteLine(ex.StackTrace);
+                return games;
+            }
+        }
+    }
+}
---
+++ SoccerApp/SoccerAppBackend/Data/IGamesTeamsOpponentsService.cs
@@ -0,0 +1,9 @@
+﻿using SoccerAppBackend.Models;
+
+namespace SoccerAppBackend.Data
+{
+    public interface IGamesTeamsOpponentsService
+    {
+        Task<List<GameTeamOpponentDto>> GetGames();
+    }
+}
