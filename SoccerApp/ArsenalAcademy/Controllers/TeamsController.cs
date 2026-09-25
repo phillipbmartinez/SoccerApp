1ef4354ea097170cebd9b05c21f48aec1e2f541b
Phillip Martinez <phillip.martinez210@gmail.com>
2026-09-25 09:53:35 -0500

    Updated index action method using new service

--- SoccerApp/ArsenalAcademy/Controllers/TeamsController.cs
+++ SoccerApp/ArsenalAcademy/Controllers/TeamsController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ArsenalAcademy.Models;
+using ArsenalAcademy.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ArsenalAcademy.Controllers
 {
     public class TeamsController : Controller
     {
-        public IActionResult Index()
+        private readonly ITeamsCoachesApiService teamsCoachesApiService;
+
+        public TeamsController(ITeamsCoachesApiService teamsCoachesApiService)
         {
-            return View();
+            this.teamsCoachesApiService = teamsCoachesApiService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            List<ViewTeamCoachViewModel> teams = await teamsCoachesApiService.GetTeams();
+
+            return View(teams);
         }
     }
 }
