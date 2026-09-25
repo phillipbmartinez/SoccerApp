3792408df24b582da20e03aab739971b32385124
Phillip Martinez <phillip.martinez210@gmail.com>
2026-09-25 09:52:22 -0500

    Created TeamsCoachesApiService class and interface

---
+++ SoccerApp/ArsenalAcademy/Services/ITeamsCoachesApiService.cs
@@ -0,0 +1,9 @@
+﻿using ArsenalAcademy.Models;
+
+namespace ArsenalAcademy.Services
+{
+    public interface ITeamsCoachesApiService
+    {
+        Task<List<ViewTeamCoachViewModel>> GetTeams();
+    }
+}
---
+++ SoccerApp/ArsenalAcademy/Services/TeamsCoachesApiService.cs
@@ -0,0 +1,34 @@
+﻿using ArsenalAcademy.Models;
+
+namespace ArsenalAcademy.Services
+{
+    public class TeamsCoachesApiService : ITeamsCoachesApiService
+    {
+        private readonly IHttpClientFactory httpClientFactory;
+
+        public TeamsCoachesApiService(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<List<ViewTeamCoachViewModel>> GetTeams()
+        {
+            HttpClient httpClient = httpClientFactory.CreateClient("SoccerAppApi");
+            List<ViewTeamCoachViewModel> teams = new List<ViewTeamCoachViewModel>();
+
+            try
+            {
+                teams = await httpClient.GetFromJsonAsync<List<ViewTeamCoachViewModel>>("teams");
+
+                return teams;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[EXCEPTION thrown from ArsenalAcademy => TeamsCoachesApiService => GetTeams: {DateTime.Now}] - Exception: {ex.Message}");
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            return teams;
+        }
+    }
+}
